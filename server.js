62f85fab53f53ff3a1e7c32d692819d1564830d6
seanryanbyu <linklayton@gmail.com>
2023-02-18 21:59:00 -0800

    change

---
+++ server.js
@@ -0,0 +1,9 @@
+// cont http = require('http');
+// const app = require('index');
+
+// const port = process.env.PORT || 5000;
+// const server = http.createServer(app);
+
+// Server.listen(port, hostname: => {
+//     console.log(`Started on port ${port}`)
+// });
