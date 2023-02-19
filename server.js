ab3fdfc1dd775eb2312e588177b68cd43f688ae9
seanryanbyu <linklayton@gmail.com>
2023-02-18 21:59:17 -0800

    change

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
-// cont http = require('http');
-// const app = require('index');
+const http = require('http');
+const app = require('index');
 
-// const port = process.env.PORT || 5000;
-// const server = http.createServer(app);
+const port = process.env.PORT || 5000;
+const server = http.createServer(app);
 
-// Server.listen(port, hostname: => {
-//     console.log(`Started on port ${port}`)
-// });
+Server.listen(port, hostname: => {
+    console.log(`Started on port ${port}`)
+});
